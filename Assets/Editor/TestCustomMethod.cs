b21d054df8c71e639edf54ab7f4ef208e4533c72
Azirel <azirel92@gmail.com>
2022-06-10 19:06:10 +0300

    Update TestCustomMethod.cs

--- Assets/Editor/TestCustomMethod.cs
+++ Assets/Editor/TestCustomMethod.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public static class TestCustomMethod
 {
-	//public static void TestMethod()
-	//{
-		//System.Diagnostics.Process.Start("cmd.exe", "echo Custom method has been invoked");
-		//Debug.Log("My custom message!!!");
-	//}
+	public static void TestMethod()
+	{
+		System.Diagnostics.Process.Start("cmd.exe", "echo Custom method has been invoked");
+		Debug.Log("My custom message!!!");
+	}
 }
